70c01c7242c3af9d7e42484297c1517a1a31b11a
Timofey Furman <timofijfurman@gmail.com>
2025-12-04 20:07:33 +0100

    ﻿Merge branch 'main' of github.com:TimofeyFurman/hillel1 into 1711-migrations # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- backend/app/apps/users/routers.py
+++ backend/app/apps/users/routers.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 
+from apps.auth.auth_handler import auth_handler
 from apps.auth.password_handler import PasswordHandler
 from apps.core.dependencies import get_async_session
 from apps.users.crud import user_manager
+from apps.users.models import User
 from apps.users.schemas import SavedUserSchema, RegisterUserSchema
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.security import OAuth2PasswordRequestForm
 
 users_router = APIRouter()
 
@@ -15,8 +18,12 @@ async def create_user(
         session: AsyncSession = Depends(get_async_session)
 ) -> SavedUserSchema:
     """create user based on name email password"""
+    maybe_user = await user_manager.get(session=session, model_field=User.email, value=user_data.email)
+    if maybe_user:
+        raise HTTPException(detail=f'User with email {user_data.email} already exists',
+                            status_code=status.HTTP_409_CONFLICT)
+
     hashed_password = await PasswordHandler.get_password_hash(user_data.password)
-    # is_valid = await PasswordHandler.verify_password(user_data.password, hashed_password)
     saved_user = await user_manager.create(
         session=session,
         name=user_data.name,
@@ -25,3 +32,22 @@ async def create_user(
     )
 
     return saved_user
+
+
+@users_router.post("/login")
+async def user_login(
+        data: OAuth2PasswordRequestForm = Depends(),
+        session: AsyncSession = Depends(get_async_session)
+) -> dict:
+    user: User = await user_manager.get(session=session, model_field=User.email, value=data.username)
+    if not user:
+        raise HTTPException(detail=f'User with email {data.username} not found',
+                            status_code=status.HTTP_404_NOT_FOUND)
+
+    is_valid_password = await PasswordHandler.verify_password(data.password, user.hashed_password)
+    if not is_valid_password:
+        raise HTTPException(detail=f'Incorrect password',
+                            status_code=status.HTTP_400_BAD_REQUEST)
+
+    tokens = await auth_handler.get_token_pairs(user)
+    return tokens
