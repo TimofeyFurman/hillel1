9516059ac9f59653ecb11640a06f5662ca007c2a
Timofey Furman <timofijfurman@gmail.com>
2025-12-11 20:07:56 +0100

    ﻿Merge branch 'main' of github.com:TimofeyFurman/hillel1 into 1711-migrations # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- backend/app/apps/auth/auth_handler.py
+++ backend/app/apps/auth/auth_handler.py
@@ -1,10 +1,14 @@
 from datetime import timedelta, datetime
-from fastapi import HTTPException, status
+from uuid import uuid4
 
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import jwt
 
+from apps.users.crud import user_manager
 from apps.users.models import User
+from redis_service import redis_service
 from settings import settings
 
 
@@ -17,14 +21,19 @@ class AuthHandler:
         access_token_payload = {
             'sub': user.email
         }
+
+        refresh_key = uuid4().hex
         refresh_token_payload = {
             'sub': user.email,
+            'key': refresh_key
         }
-        # todo process refresh token
+        redis_service.set_key(F"refresh_token_key:{refresh_key}", '1', settings.REFRESH_TOKEN_LIFETIME_MINUTES * 60)
 
         return {
-            "access_token": await self.generate_token(access_token_payload, timedelta(minutes=settings.ACCESS_TOKEN_LIFETIME_MINUTES)),
-            "refresh_token": await self.generate_token(refresh_token_payload, timedelta(minutes=settings.REFRESH_TOKEN_LIFETIME_MINUTES))
+            "access_token": await self.generate_token(access_token_payload,
+                                                      timedelta(minutes=settings.ACCESS_TOKEN_LIFETIME_MINUTES)),
+            "refresh_token": await self.generate_token(refresh_token_payload,
+                                                       timedelta(minutes=settings.REFRESH_TOKEN_LIFETIME_MINUTES))
         }
 
     async def generate_token(self, payload: dict, expiry: timedelta) -> str:
@@ -51,9 +60,28 @@ class AuthHandler:
             raise HTTPException(detail=f'token expired',
                                 status_code=status.HTTP_401_UNAUTHORIZED)
         except jwt.InvalidTokenError as e:
-            print(e, 888888888888888888)
             raise HTTPException(detail=f'where have you got this token, dude',
                                 status_code=status.HTTP_401_UNAUTHORIZED)
+
+    async def get_token_pairs_by_refresh_token(self, refresh_token: str, session: AsyncSession) -> dict:
+        payload = await self.decode_token(refresh_token)
+        refresh_token_key = payload.get("key")
+        if not refresh_token_key:
+            raise HTTPException(detail=f'Invalid refresh', status_code=status.HTTP_401_UNAUTHORIZED)
+
+        refresh_token_key_in_redis = redis_service.get(F"refresh_token_key:{refresh_token_key}")
+        if not refresh_token_key_in_redis:
+            raise HTTPException(detail=f'Redis token was used already',
+                                status_code=status.HTTP_401_UNAUTHORIZED)
+
+        redis_service.delete(F"refresh_token_key:{refresh_token_key}")
+
+        user: User = await user_manager.get(session=session, model_field=User.email, value=payload['sub'])
+        if not user:
+            raise HTTPException(detail=f'User with email {payload["sub"]} not found',
+                                status_code=status.HTTP_404_NOT_FOUND)
+        token_pair = await self.get_token_pairs(user)
+        return token_pair
 
 
 auth_handler = AuthHandler()
